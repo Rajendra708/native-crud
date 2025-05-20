3eb677a442c2f5ddccbf3a67c6200d08ffa2b9fa
rajendra708 <manisharajendrapatidar@gmail.com>
2025-05-20 12:01:25 +0530

    crud application

---
+++ data/todo.jsx
@@ -0,0 +1,23 @@
+export const data = [
+    {
+        "id":1,
+        "title":'Finish rading the book',
+        "completed": true
+    },
+    {
+        "id":2,
+        "title":'Vuy groceries',
+        "completed": false
+    },
+    {
+        "id":3,
+        "title":'write a blog post',
+        "completed": true
+    },
+    {
+        "id":4,
+        "title":'Clean the house',
+        "completed": false
+    },
+
+]
